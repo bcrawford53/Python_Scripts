0a1a910424c68d20904cbda8316dbc2b19a55018
bcrawford <bcrawford53@yahoo.com>
2026-05-22 20:18:01 -0700

    Updated Changes on 5/22/2026

--- Cisco_Python_Lab/Routing_App/Netmiko-Script-01.py
+++ Cisco_Python_Lab/Routing_App/Netmiko-Script-01.py
@@ -1,11 +1,6 @@
 from netmiko import ConnectHandler
 import json
 
-user_input = input("Enter Json File name: ")
-with open(user_input) as file:
-      json_file = file.read()
-      json_obj = json.loads(json_file)
-
 #function creation to SSH into device using Netmiko
 def Connect_to_Device(device_dict):
         with ConnectHandler(**device_dict) as session:
@@ -17,13 +12,20 @@ def Send_Commands(session, command_list):
       print(output)
       return output
 
+user_input = input("Enter Json File name: ")
+with open(user_input) as file:
+      json_file = file.read()
+      json_obj = json.loads(json_file)
+
+
 #Iterate through json file that each router is an element in the list
-for x in range(len(json_obj)):
-    dev_type = json_obj[x]['device_type']
-    devIP = json_obj[x]['hostIPaddress']
-    usr = json_obj[x]['username']
-    pwd = json_obj[x]['password']
-    enable_pwd = json_obj[x]['enable_pwd']
+for device in range(len(json_obj)):
+    dev_type = json_obj[device]['device_type']
+    devIP = json_obj[device]['hostIPaddress']
+    usr = json_obj[device]['username']
+    pwd = json_obj[device]['password']
+    enable_pwd = json_obj[device]['enable_pwd']
+    command_list = json_obj[device]['commands']
 
     #Need a function or commands to iterate through list and generate a command list????
 
@@ -34,6 +36,13 @@ for x in range(len(json_obj)):
             "secret": {enable_pwd}
     }
     sess_obj = Connect_to_Device(device_dict)
+    print(f"*"*10,"Connected to {devIP}","*"*10)
+
+    #Send the commands and print the output
+    command_output = Send_Commands(sess_obj, command_list)
+    print(command_output)
+
+
 
 
 
