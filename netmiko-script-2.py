042d3b85b6a4dbdd6c5cf30c9f7504c728a91644
bcrawford53 <107772041+bcrawford53@users.noreply.github.com>
2026-05-28 09:22:55 -0500

    Updated script with messages and comments

--- netmiko-script-2.py
+++ netmiko-script-2.py
@@ -1,12 +1,14 @@
 from netmiko import ConnectHandler
-
+#Create Constant Variables for username and password and device type
 USR = "cisco"
 PWD = "cisco"
 DEV_TYPE = "cisco_ios"
 
+#List holding the devices we are SSH into
 device_list = ["192.168.99.129", "192.168.99.130"]
 
-
+#Router class creation to create Router objects that will be SSH into and hold the device attributes
+# for SSH access by Netmiko
 class Router:
     def __init__(self, user=USR, password=PWD, device_type=DEV_TYPE, secret=PWD, host=""):
         self.device_dict = {
@@ -16,16 +18,18 @@ class Router:
             "password": password,
             "secret": secret,
         }
-
+    # connect to device
     def connect_to_device(self):
         session = ConnectHandler(**self.device_dict)
         session.enable()
         return session
 
+    # send a simple command
     def send_show_command(self, session, command):
         output = session.send_command(command, read_timeout=120)
         return output
 
+    #send a list of commands to device
     def send_config_commands(self, session, commands):
         output = session.send_config_set(commands)
         return output
