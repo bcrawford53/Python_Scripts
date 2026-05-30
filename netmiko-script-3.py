117e453373cc96c3e562a6aa80fbe888f5f59513
Brian Crawford <bcrawford53@yahoo.com>
2026-05-30 08:37:39 -0500

    Updates to Netmiko script 5-30-26

--- netmiko-script-3.py
+++ netmiko-script-3.py
@@ -1,12 +1,15 @@
 from netmiko import ConnectHandler
 
+#Login information Its better to hide credentials in environment and pull into the program
 USR = "cisco"
 PWD = "cisco"
 DEV_TYPE = "cisco_ios"
 
+#Devices that will be configured
 device_list = ["192.168.99.129", "192.168.99.130"]
 
-
+#Router class creation that will hold the attributes needed for Netmiko to use to SSH into
+#Network equipment
 class Router:
     def __init__(self, user=USR, password=PWD, device_type=DEV_TYPE, secret=PWD, host=""):
         self.device_dict = {
@@ -16,16 +19,18 @@ class Router:
             "password": password,
             "secret": secret,
         }
-
+    #Method uses Netmiko to SSH into device and returns the session
     def connect_to_device(self):
         session = ConnectHandler(**self.device_dict)
         session.enable()
         return session
 
+    #Method used to send a command to the device and returns the vty screen output
     def send_show_command(self, session, command):
         output = session.send_command(command, read_timeout=120)
         return output
-
+    
+    #Method used to send a list of commands to the device and returns the vty screen output
     def send_config_commands(self, session, commands):
         output = session.send_config_set(commands)
         return output
@@ -37,6 +42,7 @@ CPE_1 = Router(host=device_list[0])
 # Connect to device
 sess = CPE_1.connect_to_device()
 
+#Commands that will be entered into device
 command_list = ['conf t','interface g0/0', 'description Link to ISP Edge','ip address 192.168.0.2 255.255.255.0',
                 'interface loop 15','description Tunnel Source', 'ip address 165.15.15.1 255.255.255.255',
                 'exit','router bgp 65000', 'neighbor 192.168.0.1 remote-as 1000','address-family ipv4',
@@ -45,6 +51,7 @@ command_list = ['conf t','interface g0/0', 'description Link to ISP Edge','ip ad
 # Send show command
 output = CPE_1.send_config_commands(sess, command_list)
 
+#Display output to the console
 print(output)
 
 # Disconnect when finished
