b031fb90e0a652b2c9312857e59fce14ba128e7c
Brian Crawford <bcrawford53@yahoo.com>
2026-06-07 16:26:22 -0700

    Script for One device on 6-7-26

--- GETVPN_WAN_Netmiko_Script/Config_Changes/ce-1-wan-script.py
+++ GETVPN_WAN_Netmiko_Script/Config_Changes/ce-1-wan-script.py
@@ -1,7 +1,7 @@
 import os
 import yaml
 from netmiko import ConnectHandler
-import rtr_class
+from rtr_class import Router
 
 #Open YAML file to grab router and configuration information
 with open('ce-1-wan.yaml','r') as file:
@@ -9,27 +9,41 @@ with open('ce-1-wan.yaml','r') as file:
 
 
 #Create a list of commands to configure the VRF
-vrf_commands = [f'vrf definition {yaml_file['vrf']['name']}',
-                f'rd {yaml_file['vrf']['rd']}',
-                f'route-target both {yaml_file['vrf']['rt_import']}'
-                'address-family ipv4',
-                'exit']
+vrf_commands = [f"vrf definition {yaml_file['vrf']['name']}",
+                f"rd {yaml_file['vrf']['rd']}",
+                f"route-target both {yaml_file['vrf']['rt_import']}",
+                "address-family ipv4",
+                "exit"]
 
 #Create a list of commands to configure the Interface
-intf_commands = [f'interface {yaml_file['ip_address']['interfaces'][0]['interface']}',
-                 f'description {yaml_file['ip_address']['interfaces'][0]['description']}',
-                f'vrf forwarding {yaml_file['ip_address']['interfaces'][0]['vrf']}',
-                f'ip address {yaml_file['ip_address']['interfaces'][0]['address'], yaml_file['ip_address']['interfaces'][0]['mask']}',
-                f'interface {yaml_file['ip_address']['interfaces'][1]['interface']}',
-                f'description {yaml_file['ip_address']['interfaces'][1]['description']}',
-                f'ip address {yaml_file['ip_address']['interfaces'][1]['address'], yaml_file['ip_address']['interfaces'][1]['mask']}',
-                ]
+intf_commands = [f"interface {yaml_file['ip_address']['interfaces'][0]['interface']}",
+                 f"description {yaml_file['ip_address']['interfaces'][0]['description']}",
+                f"vrf forwarding {yaml_file['ip_address']['interfaces'][0]['vrf']}",
+                f"ip address {yaml_file['ip_address']['interfaces'][0]['address']} {yaml_file['ip_address']['interfaces'][0]['mask']}",
+                "no shutdown",
+                "exit",
+                f"interface {yaml_file['ip_address']['interfaces'][1]['interface']}",
+                f"description {yaml_file['ip_address']['interfaces'][1]['description']}",
+                f"ip address {yaml_file['ip_address']['interfaces'][1]['address']} {yaml_file['ip_address']['interfaces'][1]['mask']}",
+                "exit"]
 
 #BGP Configuration
-bgp_commands = [f'router bgp {yaml_file['bgp']['as_number']}',
-                f'address-family ipv4 vrf {yaml_file['bgp']['vrf']}',
-                f'neighbor {yaml_file['bgp']['neighbors'][0]['ip']} remote-as {yaml_file['bgp']['neighbors'][0]['remote-as']}',
-                'end']
+bgp_commands = [f"router bgp {yaml_file['bgp']['as_number']}",
+                f"address-family ipv4 vrf {yaml_file['bgp']['vrf']}",
+                f"neighbor {yaml_file['bgp']['neighbors'][0]['ip']} remote-as {yaml_file['bgp']['neighbors'][0]['remote_as']}",
+                "end"]
 
 #Create Router object for Netmiko
-CE-WAN-1 = rtr_class.Router(user="cisco", password="cisco", device_type="cisco_ios", host=yaml_file['host'])
+RTR_1 = Router(user="cisco", password="cisco", device_type="cisco_ios", host=yaml_file['host'], secret="cisco")
+session = RTR_1.connectToDevice()
+vrf_output = RTR_1.sendCommandSet(session, vrf_commands)
+intf_output = RTR_1.sendCommandSet(session, intf_commands)
+bgp_output = RTR_1.sendCommandSet(session, bgp_commands)   
+
+print(vrf_output)
+print('*' * 50)
+print(intf_output)
+print('*' * 50)
+print(bgp_output)
+print('*' * 50)
+
---
+++ GETVPN_WAN_Netmiko_Script/Config_Changes/rtr_class.py
@@ -0,0 +1,28 @@
+from netmiko import ConnectHandler
+
+class Router:
+    def __init__(self, user, password, device_type, secret, host):
+        self.device_dict = {"device_type": device_type,
+                            "host": host,
+                            "username": user,
+                            "password": password,
+                            "secret": secret }
+    
+    #Netmiko function to SSH to device
+    def connectToDevice(self):
+        session = ConnectHandler(**self.device_dict)
+        session.enable()
+        return session
+    
+    #Netmiko method to send a command
+    def sendCommand(self, session, command):
+        command_output = session.send_command(command)
+        return command_output
+    
+    #Method to send multiple commands, object will need to be a list
+    def sendCommandSet(self,session, command_list):
+        list_output = session.send_config_set(command_list)
+        return list_output
+
+if __name__ == "__main___":
+    print("This is a script for my Router class!")
