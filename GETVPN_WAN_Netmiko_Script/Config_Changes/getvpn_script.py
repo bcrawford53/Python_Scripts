02b2e0feeab489b17e499e59f025253d7ab3585a
Brian Crawford <bcrawford53@yahoo.com>
2026-06-13 11:44:05 -0700

    Update configs

--- GETVPN_WAN_Netmiko_Script/Config_Changes/getvpn_script.py
+++ GETVPN_WAN_Netmiko_Script/Config_Changes/getvpn_script.py
@@ -2,22 +2,21 @@ import os
 from netmiko import ConnectHandler
 from rtr_class import Router
 
-
-IP_LIST = ['192.168.254.1','192.168.254.2','192.168.254.3']
-
 #Create Router object for Netmiko
-CUST_WAN_1 = Router(user="cisco", password="cisco", device_type="cisco_ios", host=IP_LIST[0], secret="cisco")
-CUST_WAN_2 = Router(user="cisco", password="cisco", device_type="cisco_ios", host=IP_LIST[1], secret="cisco")
-CUST_WAN_3 = Router(user="cisco", password="cisco", device_type="cisco_ios", host=IP_LIST[2], secret="cisco")
+CUST_WAN_1 = Router(user="cisco", password="cisco", device_type="cisco_ios", host="192.168.254.1", secret="cisco")
+CUST_WAN_2 = Router(user="cisco", password="cisco", device_type="cisco_ios", host="192.168.254.2", secret="cisco")
+CUST_WAN_3 = Router(user="cisco", password="cisco", device_type="cisco_ios", host="192.168.254.3", secret="cisco")
+KEYSERVER = Router(user="cisco", password="cisco", device_type="cisco_ios", host="192.168.254.4", secret="cisco")
 
 WAN_1_COMM_LIST = CUST_WAN_1.openCommandFile('ce1-config.txt')
 WAN_2_COMM_LIST = CUST_WAN_2.openCommandFile('ce2-config.txt')
 WAN_3_COMM_LIST = CUST_WAN_3.openCommandFile('ce3-config.txt')
+KEYSERVER_COMM_LIST = KEYSERVER.openCommandFile('keyserver-config.txt')
 
 WAN_1_OUTPUT = CUST_WAN_1.sendCommandSet(CUST_WAN_1.connectToDevice(), WAN_1_COMM_LIST)
 WAN_2_OUTPUT = CUST_WAN_2.sendCommandSet(CUST_WAN_2.connectToDevice(), WAN_2_COMM_LIST)
 WAN_3_OUTPUT = CUST_WAN_3.sendCommandSet(CUST_WAN_3.connectToDevice(), WAN_3_COMM_LIST)
-
+KEYSERVER_OUTPUT = KEYSERVER.sendCommandSet(KEYSERVER.connectToDevice(), KEYSERVER_COMM_LIST)
 
 print(WAN_1_OUTPUT)
 print('*' * 50)
@@ -25,4 +24,4 @@ print(WAN_2_OUTPUT)
 print('*' * 50)
 print(WAN_3_OUTPUT)
 print('*' * 50)
-
+print(KEYSERVER_OUTPUT)
