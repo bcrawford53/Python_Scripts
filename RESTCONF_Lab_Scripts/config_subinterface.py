8d8201b17d0c047119ef45f62df1701de88debcd
Brian Crawford <bcrawford53@yahoo.com>
2026-07-05 17:06:50 -0700

    subinterface config RESTCONF

--- RESTCONF_Lab_Scripts/config_subinterface.py
+++ RESTCONF_Lab_Scripts/config_subinterface.py
@@ -7,31 +7,48 @@ try:
     pass
     for device, list in DEVICES.items():
         dev_list = list
-        intf_url = f"https://{dev_list['ip']}/restconf/data/Cisco-IOS-XE-native:native/interface/GigabitEthernet={list['interfaces']['GigabitEthernet2']['name']}/shutdown"
+        host = dev_list['ip']
+        intf_num = dev_list['interfaces']['GigabitEthernet2']['name']
+        USR = dev_list['usr']
+        PASS = dev_list['passwd']
+        intf_url = f"https://{host}/restconf/data/Cisco-IOS-XE-native:native/interface/GigabitEthernet={intf_num}/shutdown"
         #Enable the interface
         headers = {"Accept":"application/yang-data+json", "Content-Type":"application/yang-data+json"}
-        response = requests.request("DELETE", url=intf_url, auth=HTTPBasicAuth(dev_list['usr'],dev_list['passwd']),
+        response = requests.request("DELETE", url=intf_url, auth=HTTPBasicAuth(USR,PASS),
                                     headers=headers, verify=False)
+        print(response.status_code)
 
         #Create a Subinterface for VLAN 90
-        subintf_url = f"https://{dev_list['ip']}/restconf/data/Cisco-IOS-XE-native:native/interface/GigabitEthernet={list['interfaces']['GigabitEthernet2']['name']}"
-        IP = dev_list['interface']['GigabitEthernet2']['ip']
-        MASK = dev_list['interface']['GigabitEthernet2']['mask']
-        payload = {"Cisco-IOS-XE-native:GigabitEthernet": {"name":"2.90", "description":"LAN Subinterface",
-                                                           "encapsulation": {"vlan-id":90},
-                                                           "ip":{"address": {
-                                                               {"primary":
-                                                                    {"address":IP,
-                                                                     "mask":MASK}
-                                                               }
-                                                           }}}}
-        sub_response = requests.request("PATCH",url=subintf_url,auth=HTTPBasicAuth(dev_list['usr'],dev_list['passwd']),
+        sub_url = f"https://{host}/restconf/data/Cisco-IOS-XE-native:native/interface"
+        IP = dev_list['interfaces']['GigabitEthernet2']['ip']
+        MASK = dev_list['interfaces']['GigabitEthernet2']['mask']
+        payload = {"Cisco-IOS-XE-native:interface": {
+                        "GigabitEthernet": [
+                            {
+                                "name": "2.90",
+                                "description": "RESTCONF VLAN 90 Subinterface",
+                                "encapsulation": {
+                                    "dot1Q": {
+                                        "vlan-id": 90
+                                    }
+                                },
+                                "ip": {
+                                    "address": {
+                                        "primary": {
+                                            "address": IP,
+                                            "mask": MASK
+                                        }
+                                    }
+                                }
+                            }
+                        ]
+                    }
+        }       
+
+        sub_response = requests.request("PATCH",url=sub_url,auth=HTTPBasicAuth(USR,PASS),
                                     headers=headers, json=payload, verify=False)
-        if response == 200:
-            print("Created subinterface")
-        else:
-            print("Something broke")
-                                                        
+        print()
+        print(sub_response.status_code)
 
 except Exception as e:
     print("Error {e}")
