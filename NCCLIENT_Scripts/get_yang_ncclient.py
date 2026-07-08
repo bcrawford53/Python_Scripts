ae580f234abd9e3013e93ee859f668c1e87dc50d
Brian Crawford <bcrawford53@yahoo.com>
2026-07-07 19:35:38 -0700

    Switch RESTCONF LABBING

--- NCCLIENT_Scripts/get_yang_ncclient.py
+++ NCCLIENT_Scripts/get_yang_ncclient.py
@@ -3,11 +3,16 @@ from ncclient.operations import RPCError
 import os
 
 try:
-    with manager.connect(host="192.168.99.128", port=830, username="cisco", password="cisco",
+    with manager.connect(host="192.168.99.155", port=830, username="netadmin", password="cisco123",
                          timeout=30, device_params={'name':'iosxe'}, hostkey_verify=False) as m:
         print("Connected to device")
         for capability in m.server_capabilities:
             print(capability)
+            capability_list =[]
+            capability_list.append(capability)
+        with open("capabilities.txt", "a") as f:
+            for capability in capability_list:
+                f.write(capability + "\n")
 except RPCError as rpc_error:
     print(f"RPC Error: {rpc_error}")
 except Exception as error:
